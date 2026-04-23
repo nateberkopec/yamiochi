941798f9310ede6a948f0fc17c2fd1504ad49f5c
Nate Berkopec <nate.berkopec@gmail.com>
2026-04-23 19:24:37 +0900

    Load rackup apps and return HTTP responses (#13)
    Co-authored-by: yamiochi-factory <yamiochi-factory@speedshop.co>

--- lib/yamiochi/server.rb
+++ lib/yamiochi/server.rb
@@ -1,11 +1,49 @@
 # frozen_string_literal: true
 
 require "socket"
+require "stringio"
+require "time"
 
 module Yamiochi
   class Server
     DEFAULT_HOST = "0.0.0.0"
     DEFAULT_PORT = 9292
+    MAX_HEADER_BYTES = 16 * 1024
+
+    STATUS_REASONS = {
+      200 => "OK",
+      400 => "Bad Request",
+      500 => "Internal Server Error"
+    }.freeze
+
+    class BadRequestError < StandardError; end
+
+    class RackupLoader
+      def self.load(path)
+        new(path).load
+      end
+
+      def initialize(path)
+        @path = path
+        @app = nil
+      end
+
+      def load
+        instance_eval(File.read(path), path, 1)
+
+        return app if app
+
+        raise ArgumentError, "Rackup file did not call run: #{path}"
+      end
+
+      def run(configured_app)
+        @app = configured_app
+      end
+
+      private
+
+      attr_reader :path, :app
+    end
 
     attr_reader :rackup_path, :host, :port, :bound_port
 
@@ -20,6 +58,7 @@ module Yamiochi
 
     def run
       validate_rackup_path!
+      rack_app
       boot
       self
     end
@@ -58,7 +97,13 @@ module Yamiochi
 
       begin
         enable_tcp_nodelay(client)
-        read_request_bytes(client)
+        request_bytes = read_request_bytes(client)
+        return unless request_bytes
+
+        request_env = build_request_env(listener, request_bytes)
+        write_rack_response(client, *call_app(request_env))
+      rescue BadRequestError
+        write_simple_response(client, 400)
       ensure
         close_socket(client)
       end
@@ -69,9 +114,179 @@ module Yamiochi
     end
 
     def read_request_bytes(client)
-      client.readpartial(1024)
+      request_bytes = String.new.b
+
+      loop do
+        request_bytes << client.readpartial(1024)
+
+        return request_bytes if request_bytes.include?("\r\n\r\n")
+
+        if request_bytes.bytesize > MAX_HEADER_BYTES
+          raise BadRequestError, "request headers exceed #{MAX_HEADER_BYTES} bytes"
+        end
+      end
     rescue EOFError
-      nil
+      return nil if request_bytes.empty?
+
+      request_bytes
+    end
+
+    def build_request_env(listener, request_bytes)
+      unless request_bytes.include?("\r\n\r\n")
+        raise BadRequestError, "incomplete request headers"
+      end
+
+      header_block, = request_bytes.split("\r\n\r\n", 2)
+      request_line, *header_lines = header_block.split("\r\n")
+      method, request_target, server_protocol = parse_request_line(request_line)
+      headers = parse_headers(header_lines)
+      path_info, query_string = split_request_target(request_target)
+
+      {
+        "REQUEST_METHOD" => method,
+        "SCRIPT_NAME" => "",
+        "PATH_INFO" => path_info,
+        "QUERY_STRING" => query_string,
+        "SERVER_NAME" => server_name(listener, headers["Host"]),
+        "SERVER_PORT" => listener.local_address.ip_port.to_s,
+        "SERVER_PROTOCOL" => server_protocol,
+        "rack.version" => [3, 0],
+        "rack.url_scheme" => "http",
+        "rack.input" => StringIO.new(String.new.b),
+        "rack.errors" => err,
+        "rack.multithread" => false,
+        "rack.multiprocess" => true,
+        "rack.run_once" => false
+      }.merge(rack_headers(headers))
+    end
+
+    def parse_request_line(request_line)
+      method, request_target, server_protocol = request_line.to_s.split(" ", 3)
+
+      if [method, request_target, server_protocol].any?(&:nil?)
+        raise BadRequestError, "malformed request line"
+      end
+
+      unless server_protocol == "HTTP/1.1"
+        raise BadRequestError, "unsupported protocol: #{server_protocol}"
+      end
+
+      [method, request_target, server_protocol]
+    end
+
+    def parse_headers(header_lines)
+      header_lines.each_with_object({}) do |line, headers|
+        name, value = line.split(":", 2)
+        raise BadRequestError, "malformed header: #{line.inspect}" unless name && value
+
+        headers[name] = value.lstrip
+      end
+    end
+
+    def split_request_target(request_target)
+      path_info, query_string = request_target.split("?", 2)
+      raise BadRequestError, "unsupported request target: #{request_target}" unless path_info&.start_with?("/")
+
+      [path_info, query_string.to_s]
+    end
+
+    def server_name(listener, host_header)
+      return host_header_name(host_header) if host_header
+
+      listener.local_address.ip_address
+    end
+
+    def host_header_name(host_header)
+      if host_header.start_with?("[")
+        host_header[/\A\[(?<host>[^\]]+)\](?::\d+)?\z/, :host] || host_header
+      else
+        host_header.split(":", 2).first
+      end
+    end
+
+    def rack_headers(headers)
+      headers.each_with_object({}) do |(name, value), env|
+        env[env_header_name(name)] = value
+      end
+    end
+
+    def env_header_name(header_name)
+      normalized_name = header_name.upcase.tr("-", "_")
+      return normalized_name if %w[CONTENT_LENGTH CONTENT_TYPE].include?(normalized_name)
+
+      "HTTP_#{normalized_name}"
+    end
+
+    def call_app(request_env)
+      rack_app.call(request_env)
+    rescue StandardError
+      [500, {}, []]
+    end
+
+    def rack_app
+      @rack_app ||= RackupLoader.load(rackup_path)
+    end
+
+    def write_simple_response(client, status)
+      write_rack_response(client, status, {}, [])
+    end
+
+    def write_rack_response(client, status, headers, body)
+      response_status = Integer(status)
+      response_headers = headers || {}
+      response_body = read_response_body(body)
+      normalized_headers = build_response_headers(response_headers, response_body)
+
+      client.write("HTTP/1.1 #{response_status} #{reason_phrase(response_status)}\r\n")
+      normalized_headers.each do |name, value|
+        client.write("#{name}: #{value}\r\n")
+      end
+      client.write("\r\n")
+      client.write(response_body)
+    ensure
+      body.close if body.respond_to?(:close)
+    end
+
+    def read_response_body(body)
+      response_body = String.new.b
+      body.each do |chunk|
+        response_body << chunk.to_s
+      end
+      response_body
+    end
+
+    def build_response_headers(headers, response_body)
+      response_headers = headers.each_with_object({}) do |(name, value), normalized_headers|
+        normalized_headers[name.to_s] = value.to_s
+      end
+
+      set_response_header!(response_headers, "Server", "Yamiochi")
+      set_response_header!(response_headers, "Connection", "close")
+      set_response_header!(response_headers, "Date", Time.now.httpdate)
+
+      unless response_header?(response_headers, "Content-Length") || response_header?(response_headers, "Transfer-Encoding")
+        response_headers["Content-Length"] = response_body.bytesize.to_s
+      end
+
+      response_headers
+    end
+
+    def set_response_header!(headers, name, value)
+      delete_response_header!(headers, name)
+      headers[name] = value
+    end
+
+    def response_header?(headers, name)
+      headers.any? { |header_name, _| header_name.casecmp?(name) }
+    end
+
+    def delete_response_header!(headers, name)
+      existing_name, = headers.find { |header_name, _| header_name.casecmp?(name) }
+      headers.delete(existing_name) if existing_name
+    end
+
+    def reason_phrase(status)
+      STATUS_REASONS.fetch(status, "Unknown")
     end
 
     def close_socket(socket)
--- test/unit/executable_test.rb
+++ test/unit/executable_test.rb
@@ -7,23 +7,26 @@ require "tmpdir"
 require_relative "../test_helper"
 
 class YamiochiExecutableTest < Minitest::Test
-  def test_executable_serves_one_connection_and_exits_successfully
+  def test_executable_serves_one_http_response_and_exits_successfully
     ensure_default_port_available
 
     Dir.mktmpdir("yamiochi-exe-test") do |dir|
       config_ru = File.join(dir, "config.ru")
-      File.write(config_ru, rackup_contents)
+      File.write(config_ru, rackup_contents("hello from exe"))
 
       stdout_text = nil
       stderr_text = nil
       status = nil
+      response_text = nil
 
       Open3.popen3(executable_path, config_ru, chdir: repo_root) do |stdin, stdout, stderr, wait_thr|
         stdin.close
 
         begin
-          connect_when_ready("127.0.0.1", Yamiochi::Server::DEFAULT_PORT, wait_thr) do |client|
-            client.write("GET / HTTP/1.1\r\nHost: localhost\r\n\r\n")
+          response_text = connect_when_ready("127.0.0.1", Yamiochi::Server::DEFAULT_PORT, wait_thr) do |client|
+            client.write(basic_request("/"))
+            client.close_write
+            client.read
           end
 
           status = wait_for_exit(wait_thr)
@@ -37,13 +40,46 @@ class YamiochiExecutableTest < Minitest::Test
       assert status.success?, "Expected executable to exit successfully, stdout: #{stdout_text.inspect}, stderr: #{stderr_text.inspect}"
       assert_empty stdout_text
       assert_empty stderr_text
+      assert_equal "HTTP/1.1 200 OK", response_status_line(response_text)
+      assert_equal "hello from exe", response_body(response_text)
+
+      headers = response_headers(response_text)
+      assert_equal "Yamiochi", headers.fetch("Server")
+      assert_equal "close", headers.fetch("Connection")
+      assert_equal "14", headers.fetch("Content-Length")
+      assert headers.key?("Date"), "Expected response to include a Date header"
     end
   end
 
   private
 
-  def rackup_contents
-    "run ->(_env) { [200, {}, ['ok']] }\n"
+  def rackup_contents(body)
+    "run ->(_env) { [200, {}, [#{body.dump}]] }\n"
+  end
+
+  def basic_request(target)
+    "GET #{target} HTTP/1.1\r\nHost: localhost\r\n\r\n"
+  end
+
+  def response_status_line(response_text)
+    response_head(response_text).lines.first.chomp
+  end
+
+  def response_headers(response_text)
+    response_head(response_text).lines.drop(1).each_with_object({}) do |line, headers|
+      name, value = line.chomp.split(": ", 2)
+      headers[name] = value
+    end
+  end
+
+  def response_body(response_text)
+    _head, body = response_text.split("\r\n\r\n", 2)
+    body
+  end
+
+  def response_head(response_text)
+    head, _body = response_text.split("\r\n\r\n", 2)
+    head
   end
 
   def repo_root
