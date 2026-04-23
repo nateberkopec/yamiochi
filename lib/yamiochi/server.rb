3fcab0378ee8f8d00077c6778ba468cb072c81da
yamiochi-factory <yamiochi-factory@speedshop.co>
2026-04-23 10:11:39 +0000

    Bind the default TCP listener

--- lib/yamiochi/server.rb
+++ lib/yamiochi/server.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
+require "socket"
+
 module Yamiochi
   class Server
-    attr_reader :rackup_path
+    DEFAULT_HOST = "0.0.0.0"
+    DEFAULT_PORT = 9292
 
-    def initialize(rackup_path:, out: $stdout, err: $stderr)
+    attr_reader :rackup_path, :host, :port, :bound_port
+
+    def initialize(rackup_path:, host: DEFAULT_HOST, port: DEFAULT_PORT, out: $stdout, err: $stderr)
       @rackup_path = File.expand_path(rackup_path.to_s)
+      @host = host
+      @port = Integer(port)
+      @bound_port = nil
       @out = out
       @err = err
     end
@@ -27,11 +35,54 @@ module Yamiochi
     end
 
     def boot
-      # Follow-up issues extend this shared boot path to bind listeners and load
-      # the Rack app from `rackup_path`.
+      listener = nil
+
+      begin
+        listener = open_listener
+        handle_single_client(listener)
+      ensure
+        close_socket(listener)
+        flush_streams
+      end
+    end
+
+    def open_listener
+      listener = TCPServer.new(host, port)
+      listener.listen(1024)
+      @bound_port = listener.local_address.ip_port
+      listener
+    end
+
+    def handle_single_client(listener)
+      client = listener.accept
+
+      begin
+        enable_tcp_nodelay(client)
+        read_request_bytes(client)
+      ensure
+        close_socket(client)
+      end
+    end
+
+    def enable_tcp_nodelay(client)
+      client.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
+    end
+
+    def read_request_bytes(client)
+      client.readpartial(1024)
+    rescue EOFError
+      nil
+    end
+
+    def close_socket(socket)
+      socket&.close
+    rescue IOError
+      nil
+    end
+
+    def flush_streams
       out.flush
       err.flush
-      true
     end
   end
 end
--- test/unit/server_test.rb
+++ test/unit/server_test.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "socket"
 require "stringio"
 require "tmpdir"
 
@@ -9,7 +10,7 @@ class YamiochiServerTest < Minitest::Test
   def test_initialization_normalizes_rackup_path
     Dir.mktmpdir("yamiochi-server-test") do |dir|
       config_ru = File.join(dir, "config.ru")
-      File.write(config_ru, "run ->(_env) { [200, {}, ['ok']] }\n")
+      File.write(config_ru, rackup_contents)
       relative_path = File.join(dir, ".", "config.ru")
 
       server = Yamiochi::Server.new(rackup_path: relative_path, out: StringIO.new, err: StringIO.new)
@@ -18,14 +19,59 @@ class YamiochiServerTest < Minitest::Test
     end
   end
 
-  def test_run_completes_for_a_valid_rackup_file
+  def test_run_binds_and_handles_one_client_connection
     Dir.mktmpdir("yamiochi-server-test") do |dir|
       config_ru = File.join(dir, "config.ru")
-      File.write(config_ru, "run ->(_env) { [200, {}, ['ok']] }\n")
+      File.write(config_ru, rackup_contents)
 
-      server = Yamiochi::Server.new(rackup_path: config_ru, out: StringIO.new, err: StringIO.new)
+      server = Yamiochi::Server.new(
+        rackup_path: config_ru,
+        host: "127.0.0.1",
+        port: 0,
+        out: StringIO.new,
+        err: StringIO.new
+      )
 
-      assert_same server, server.run
+      thread = Thread.new do
+        Thread.current.report_on_exception = false
+        server.run
+      end
+
+      bound_port = wait_for_bound_port(server, thread)
+
+      TCPSocket.open("127.0.0.1", bound_port) do |client|
+        client.write("GET / HTTP/1.1\r\nHost: localhost\r\n\r\n")
+      end
+
+      assert thread.join(5), "Expected server thread to exit after handling one client"
+      assert_same server, thread.value
+      assert_equal bound_port, server.bound_port
+      assert_operator server.bound_port, :>, 0
     end
+  end
+
+  private
+
+  def rackup_contents
+    "run ->(_env) { [200, {}, ['ok']] }\n"
+  end
+
+  def wait_for_bound_port(server, thread, timeout: 5)
+    deadline = Process.clock_gettime(Process::CLOCK_MONOTONIC) + timeout
+
+    loop do
+      return server.bound_port if server.bound_port
+
+      unless thread.alive?
+        assert_same server, thread.value
+        flunk "Server thread exited before publishing its bound port"
+      end
+
+      break if Process.clock_gettime(Process::CLOCK_MONOTONIC) >= deadline
+
+      sleep 0.01
+    end
+
+    flunk "Timed out waiting for server to bind a port"
   end
 end
